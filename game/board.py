36ba61ff0887b050d6a0df425f49da01bbadbf35
jcorvino <jcorvino@users.noreply.github.com>
2019-07-09 22:44:00 -0400

    Created game board.

--- game/board.py
+++ game/board.py
@@ -1,8 +1,45 @@
+from game.pieces import King, Queen, Rook, Bishop, Knight, Pawn
+
+
 class GameBoard:
     """
     Chess game board (8x8).
 
     Columns span from A to H. Rows span from 1 to 8.
     """
+
+    initial_white_positions = {
+        King: ('E1',),
+        Queen: ('D1',),
+        Rook: ('A1', 'H1',),
+        Bishop: ('C1', 'F1',),
+        Knight: ('B1', 'G1',),
+        Pawn: (i + '2' for i in 'ABCDEFGH'),
+    }
+
+    initial_black_positions = {
+        King: ('E8',),
+        Queen: ('D8',),
+        Rook: ('A8', 'H8',),
+        Bishop: ('C8', 'F8',),
+        Knight: ('B8', 'G8',),
+        Pawn: (i + '7' for i in 'ABCDEFGH'),
+    }
+
     def __init__(self):
-        pass
+        """
+        Create starting game board
+        """
+
+        # List containing all active board pieces
+        self.pieces = []
+
+        # Populate white pieces on game board
+        for piece_type, locations in self.initial_white_positions.items():
+            for location in locations:
+                self.pieces.append(piece_type('white', location))
+
+        # Populate black pieces on game board
+        for piece_type, locations in self.initial_black_positions.items():
+            for location in locations:
+                self.pieces.append(piece_type('black', location))
--- game/chess.py
+++ game/chess.py
@@ -6,7 +6,8 @@ class Chess:
     Main game logic
     """
     def __init__(self):
-        board = GameBoard()
+        self.board = GameBoard()
 
     def play(self):
         print('Let\'s play a game of chess!')
+        print(list(map(lambda x: (x.color + '-' + x.name, x.location), self.board.pieces)))
