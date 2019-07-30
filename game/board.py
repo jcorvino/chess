673d9ec0e0a3f681cfb84798e2ad846701a1a772
jcorvino <jcorvino@berkeley.edu>
2019-07-29 21:46:00 -0400

    Updated game board logic

--- game/board.py
+++ game/board.py
@@ -1,45 +1,84 @@
-from game.pieces import King, Queen, Rook, Bishop, Knight, Pawn
+import re
+from pieces import King, Queen, Rook, Bishop, Knight, Pawn
+
+
+class BoardDict(dict):
+	"""
+	Special dictionary that represents a chess game board
+	"""
+	def __init__(self):
+		"""
+		Auto load the keys for the game board (A1-H8)
+		"""
+		rows = 'ABCDEFGH'
+		cols = '12345678'
+		d = dict()
+		for row in rows:
+			for col in cols:
+				d[row + col] = None
+		super().__init__(d)
+	
+	def __setitem__(self, key, value):
+		"""
+		Prevent user from modifying keys
+		"""
+		if not re.match('[A-H][1-8]', key):
+			raise KeyError(f'Board can only have positions A1-H8, not {key}')
+		super().__setitem__(key, value)
 
 
 class GameBoard:
-    """
-    Chess game board (8x8).
+	"""
+	Chess game board (8x8).
 
-    Columns span from A to H. Rows span from 1 to 8.
-    """
+	Columns span from A to H. Rows span from 1 to 8.
+	"""
+	_rows = 'ABCDEFGH'
+	_cols = '12345678'
+	
+	initial_white_positions = {
+		King: ('E1',),
+		Queen: ('D1',),
+		Rook: ('A1', 'H1',),
+		Bishop: ('C1', 'F1',),
+		Knight: ('B1', 'G1',),
+		Pawn: (i + '2' for i in _rows),
+	}
 
-    initial_white_positions = {
-        King: ('E1',),
-        Queen: ('D1',),
-        Rook: ('A1', 'H1',),
-        Bishop: ('C1', 'F1',),
-        Knight: ('B1', 'G1',),
-        Pawn: (i + '2' for i in 'ABCDEFGH'),
-    }
+	initial_black_positions = {
+		King: ('E8',),
+		Queen: ('D8',),
+		Rook: ('A8', 'H8',),
+		Bishop: ('C8', 'F8',),
+		Knight: ('B8', 'G8',),
+		Pawn: (i + '7' for i in _rows),
+	}
 
-    initial_black_positions = {
-        King: ('E8',),
-        Queen: ('D8',),
-        Rook: ('A8', 'H8',),
-        Bishop: ('C8', 'F8',),
-        Knight: ('B8', 'G8',),
-        Pawn: (i + '7' for i in 'ABCDEFGH'),
-    }
+	def __init__(self):
+		"""
+		Create starting game board
+		"""
+		self.board = BoardDict()  # empty board
+		self.history = list()  # list of all past moves
 
-    def __init__(self):
-        """
-        Create starting game board
-        """
+		# Populate white pieces on game board
+		for piece_type, locations in self.initial_white_positions.items():
+			for location in locations:
+				self.board[location] = piece_type('white')
 
-        # List containing all active board pieces
-        self.pieces = []
+		# Populate black pieces on game board
+		for piece_type, locations in self.initial_black_positions.items():
+			for location in locations:
+				self.board[location] = piece_type('black')
 
-        # Populate white pieces on game board
-        for piece_type, locations in self.initial_white_positions.items():
-            for location in locations:
-                self.pieces.append(piece_type('white', location))
+	def get_moves(location):
+		"""
+		Gets the allowed moves for a piece in the specified location
+		"""
+		pass
 
-        # Populate black pieces on game board
-        for piece_type, locations in self.initial_black_positions.items():
-            for location in locations:
-                self.pieces.append(piece_type('black', location))
+
+if __name__ == '__main__':
+	print('test')
+	mygb = GameBoard()
+	print(mygb.board)
--- game/pieces.py
+++ game/pieces.py
@@ -2,93 +2,82 @@ import re
 
 
 class GamePiece:
-    """
-    Game piece class.
+	"""
+	Game piece class.
 
-    Base class for all chess pieces.
-    """
-    names = (
-        'king',
-        'queen',
-        'rook',
-        'bishop',
-        'knight',
-        'pawn',
-    )
+	Base class for all chess pieces.
+	"""
+	names = (
+		'king',
+		'queen',
+		'rook',
+		'bishop',
+		'knight',
+		'pawn',
+	)
 
-    colors = (
-        'black',
-        'white',
-    )
+	colors = (
+		'black',
+		'white',
+	)
 
-    def __init__(self, name, color, location):
-        self._name = None
-        self._color = None
-        self._location = None
+	def __init__(self, name, color):
+		self._name = None
+		self._color = None
 
-        self.name = name
-        self.color = color
-        self.location = location
-        self.has_moved = False
+		self.name = name
+		self.color = color
+		self.has_moved = False
+	
+	def __repr__(self):
+		return type(self).__name__ + '(\'' + str(self.color) + '\')'
 
-    @property
-    def name(self):
-        return self._name
+	@property
+	def name(self):
+		return self._name
 
-    @name.setter
-    def name(self, val):
-        if val not in self.names:
-            raise ValueError('Invalid game piece name: \'%s\'' % str(val))
-        self._name = val
+	@name.setter
+	def name(self, val):
+		if val not in self.names:
+			raise ValueError('Invalid game piece name: \'%s\'' % str(val))
+		self._name = val
 
-    @property
-    def color(self):
-        return self._color
+	@property
+	def color(self):
+		return self._color
 
-    @color.setter
-    def color(self, val):
-        if val not in self.colors:
-            raise ValueError('Game piece color must be black or white not \'%s\'' % str(val))
-        self._color = val
-
-    @property
-    def location(self):
-        return self._location
-
-    @location.setter
-    def location(self, val):
-        if re.match('[A-H][1-8]', val) and val != self._location:
-            self._location = val
-            self.has_moved = True
-        else:
-            raise ValueError('Game piece moved to an invalid location: \'%s\'' % str(val))
+	@color.setter
+	def color(self, val):
+		if val not in self.colors:
+			raise ValueError('Game piece color must be black or white not \'%s\'' % str(val))
+		self._color = val
 
 
 class King(GamePiece):
-    def __init__(self, color, location):
-        super().__init__('king', color, location)
+	def __init__(self, color):
+		super().__init__('king', color)
 
 
 class Queen(GamePiece):
-    def __init__(self, color, location):
-        super().__init__('queen', color, location)
+	def __init__(self, color):
+		super().__init__('queen', color)
 
 
 class Rook(GamePiece):
-    def __init__(self, color, location):
-        super().__init__('rook', color, location)
+	def __init__(self, color):
+		super().__init__('rook', color)
 
 
 class Bishop(GamePiece):
-    def __init__(self, color, location):
-        super().__init__('bishop', color, location)
+	def __init__(self, color):
+		super().__init__('bishop', color)
 
 
 class Knight(GamePiece):
-    def __init__(self, color, location):
-        super().__init__('knight', color, location)
+	def __init__(self, color):
+		super().__init__('knight', color)
 
 
 class Pawn(GamePiece):
-    def __init__(self, color, location):
-        super().__init__('pawn', color, location)
+	def __init__(self, color):
+		super().__init__('pawn', color)
